ef968912487545372a81ce9337d760c963e798d2
Upesh Vishwakarma <upeshvishwakarma@Upeshs-MacBook-Pro.local>
2020-09-26 19:48:40 +0530

    updating new form of plugin

---
+++ my-social-tags.php
@@ -0,0 +1,144 @@
+<?php 
+/**
+ * Plugin Name: My Social Tags
+ * Plugin URI: https://github.com/upeshv/my-social-tags
+ * Description: This plugin add's Social Open Graph tags to our single posts. It enables any web page to become a rich object in a social graph. For instance, this is used on Facebook, twitter, Linkedin etc to allow any web page to have the same functionality as any other object on this social sites.
+ * Tags : Social Open Graph, Open Graph protocol, og:title, og:type, og:url , og:image
+ * Version: 2.0.0
+ * Author: Upesh Vishwakarma
+ * Author URI: https://github.com/upeshv/
+ * License: GPL2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+function mft_add_additional_fields_meta_box() {
+	add_meta_box(
+		'mft_additional_fields_meta_box', // $id
+		'Optional Metadata', // $title
+		'mft_show_additional_fields_meta_box', // $callback
+		'post', // $screen
+		'normal', // $context
+		'high' // $priority
+	);
+}
+add_action( 'add_meta_boxes', 'mft_add_additional_fields_meta_box' );
+
+function mft_show_additional_fields_meta_box() {
+	global $post;  
+		$meta = get_post_meta( $post->ID, 'additional_fields', true ); ?>
+
+	<input type="hidden" name="your_meta_box_nonce" value="<?php echo wp_create_nonce( basename(__FILE__) ); ?>">
+
+    <!-- All fields will go here -->
+	<p>
+		<label for="additional_fields[1]"><b>OG:Type</b> (Type of your object.)</label>
+		<br>
+		<input type="text" name="additional_fields[1]" id="additional_fields[1]" class="regular-text" value="<?php echo $meta['1']; ?>">		
+	</p>
+	<p>
+		<label for="additional_fields[2]"><b>OG:Description</b> (A one to two sentence description of your object.)</label>
+		<br>
+		<input type="text" name="additional_fields[2]" id="additional_fields[2]" class="regular-text" value="<?php echo $meta['2']; ?>">		
+	</p>
+	<p>
+		<label for="additional_fields[3]"><b>OG:Audio</b> (A URL to an audio file to accompany this object)</label>
+		<br>
+		<input type="text" name="additional_fields[3]" id="additional_fields[3]" class="regular-text" value="<?php echo $meta['3']; ?>">		
+	</p>
+	<p>
+		<label for="additional_fields[4]"><b>OG:Video</b> (A URL to a video file that complements this object.)</label>
+		<br>
+		<input type="text" name="additional_fields[4]" id="additional_fields[4]" class="regular-text" value="<?php echo $meta['4']; ?>">		
+	</p>
+	<p>
+		<label for="additional_fields[5]"><b>OG:Determiner</b> (The word that appears before this object's title in a sentence. An enum of (a, an, the, "", auto). If auto is chosen, the consumer of your data should chose between "a" or "an". Default is "" (blank).)</label>
+		<br>
+		<input type="text" name="additional_fields[5]" id="additional_fields[5]" class="regular-text" value="<?php echo $meta['5']; ?>">		
+	</p>
+	<p>
+		<label for="additional_fields[6]"><b>OG:Locale</b> (The locale these tags are marked up in. Of the format language_TERRITORY. Default is en_US.)</label>
+		<br>
+		<input type="text" name="additional_fields[6]" id="additional_fields[6]" class="regular-text" value="<?php echo $meta['6']; ?>">		
+	</p>
+	<p>
+		<label for="additional_fields[7]"><b>OG:Locale Alternate</b> (An array of other locales this page is available in.)</label>
+		<br>
+		<input type="text" name="additional_fields[7]" id="additional_fields[7]" class="regular-text" value="<?php echo $meta['7']; ?>">		
+	</p>
+	
+	
+<?php
+	}
+	
+
+function mft_save_additional_fields_meta( $post_id ) {   
+	// verify nonce
+	if ( !wp_verify_nonce( $_POST['your_meta_box_nonce'], basename(__FILE__) ) ) {
+		return $post_id; 
+	}
+	// check autosave
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return $post_id;
+	}
+	// check permissions
+	if ( 'page' === $_POST['post_type'] ) {
+		if ( !current_user_can( 'edit_page', $post_id ) ) {
+			return $post_id;
+		} elseif ( !current_user_can( 'edit_post', $post_id ) ) {
+			return $post_id;
+		}  
+	}
+	
+	$old = get_post_meta( $post_id, 'additional_fields', true );
+	$new = $_POST['additional_fields'];
+
+	if ( $new && $new !== $old ) {
+		update_post_meta( $post_id, 'additional_fields', $new );
+	} elseif ( '' === $new && $old ) {
+		delete_post_meta( $post_id, 'additional_fields', $old );
+	}
+}
+add_action( 'save_post', 'mft_save_additional_fields_meta', 10, 2 );
+	
+	
+function mft_my_social_tags() {
+  if( is_single() ) {
+  ?>
+  
+    <meta property="og:title" content="<?php the_title() ?>" />
+    <meta property="og:site_name" content="<?php bloginfo( 'name' ) ?>" />
+    <meta property="og:url" content="<?php the_permalink() ?>" />
+ <?php 
+      if ( has_post_thumbnail() ) :
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); 
+    ?>
+	<meta property="og:image" content="<?php echo $image[0]; ?>"/>      <?php endif; ?>	
+<?php 
+global $post;
+while (have_posts()) : the_post(); 
+$meta = get_post_meta( $post->ID, 'additional_fields', true ); ?>
+	
+<?php if($meta['1'] != ''){ ?>	<meta property="og:type" content="<?php echo $meta['1']; ?>" />   <?php } ?>
+<?php if($meta['2'] != ''){ ?>		
+	<meta property="og:description" content="<?php echo $meta['2']; ?>" />  <?php } ?>
+<?php if($meta['3'] != ''){ ?> 				
+	<meta property="og:audio" content="<?php echo $meta['3']; ?>" /> <?php } ?>
+<?php if($meta['4'] != ''){ ?> 				
+	<meta property="og:video" content="<?php echo $meta['4']; ?>" />  <?php } ?>
+<?php if($meta['5'] != ''){ ?> 		
+	<meta property="og:determiner" content="<?php echo $meta['5']; ?>" />  <?php } ?>
+<?php if($meta['6'] != ''){ ?>   			
+	<meta property="og:locale" content="<?php echo $meta['6']; ?>" />  <?php } ?>
+<?php if($meta['7'] != ''){ ?>   
+	<meta property="og:locale:alternate" content="<?php echo $meta['7']; ?>" />  <?php } ?>
+
+<?php endwhile; ?>
+	
+   
+    
+  <?php
+  }
+}
+add_action( 'wp_head', 'mft_my_social_tags' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== My Social Tags ===
+Contributors: Vishwaupesh
+Donate link: 
+Tags: Social Open Graph, Open Graph protocol, og:title, og:type, og:url , og:image
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+* Social Open Graph plugin allows you to add metadata to your page. Which turn your web pages into graph objects. This will place additional <meta> tags in the <head> of your web page.
+
+* Whenever you will create a new post page it will automatically turn your web pages into graph objects with basis information. For example : title,url,image.
+
+* You will have an options to add more metadata to your web page. 
+
+* This enables any web page to become a rich object in a social graph. For instance, this is used on any social sites to allow any web page to have the same functionality as any other object on the sites.
+
+* While many different technologies and schemas exist and could be combined together, there isn't a single technology which provides enough information to richly represent any web page within the social graph. This builds on these existing technologies and gives developers one thing to implement. 
+
+== Installation ==
+
+* Install this my-social-tags plugin and activate it, after activating. Your post page will automatically turn your web pages into graph objects with basis information. For example : title,url,image.
+
+* On every post page you will find an option to add more Metadata to your web page.
+
+== Frequently Asked Questions ==
+Write your questions and queries at [Support ](https://wordpress.org/support/plugin/My-social-Tags-Open-Graph-protocol "Write your questions and queries ")
+
+== Screenshots ==
+1. Meta Tags
+2. Screen Options
+3. Additional Metadata Fields
+
+== Changelog ==
+No recent changes
+
+== Upgrade Notice ==
+No new notices
